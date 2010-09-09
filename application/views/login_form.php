4801ddec9593a1a5c90493f6ea4f092548ac7759
Karthik <=>
2010-09-10 00:56:19 +0530

    login form

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,8 +1,13 @@
 <div id="main">
-    <?php echo form_open('login/validate'); ?>
-    <label for="username">Username: </label>
-    <?php echo form_input('username'); ?>
-    <label for="password">Password: </label>
-    <?php echo form_password('password'); ?>
-    <?php echo form_submit('login', 'Login'); ?>
+    <div id="login_box" class="box_gradient box_shadow">
+        <h2 class="textshadow">Login</h2>
+        <?php echo validation_errors('<div class="errors"><p>','<a href="#">X</a></p></div>'); ?>
+        <?php echo form_open('login/validate'); ?>
+        <label for="username">Username: </label>
+        <?php echo form_input('username'); ?>
+        <label for="password">Password: </label>
+        <?php echo form_password('password'); ?>
+        <?php echo form_submit('login', 'Login'); ?>
+        <?php echo form_close(); ?>
+    </div>
 </div>
