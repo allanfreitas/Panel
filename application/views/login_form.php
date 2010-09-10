84957ac898ba17dffd10b0086abcbef9a71f92c1
Karthik <karthik@gsoftmysore.com>
2010-09-10 23:26:28 +0530

    now login form show validation errors

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -3,11 +3,13 @@
         <h2 class="textshadow">Login</h2>
         <?php echo validation_errors('<div class="errors"><p>','<a href="#">X</a></p></div>'); ?>
         <?php echo form_open('login/validate'); ?>
-        <label for="username">Username: </label>
-        <?php echo form_input('username'); ?>
-        <label for="password">Password: </label>
-        <?php echo form_password('password'); ?>
+        <?php echo form_input('username', 'Username'); ?>
+        <?php echo form_password('password', 'Password'); ?>
         <?php echo form_submit('login', 'Login'); ?>
+        <?php
+            $attr = array('id' => 'signup');
+            echo anchor('signup', 'Create Account', $attr);
+        ?>
         <?php echo form_close(); ?>
     </div>
 </div>
